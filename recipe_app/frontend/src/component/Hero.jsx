b99b3a4075419cd73f899779628cda885b1265d1
Blessman-Newton <blessmannewton0@gmail.com>
2023-08-31 20:43:39 +0100

    Update here

--- recipe_app/frontend/src/component/Hero.jsx
+++ recipe_app/frontend/src/component/Hero.jsx
@@ -4,7 +4,6 @@ import 'swiper/swiper-bundle.min.css';
 import SwiperCore, { Autoplay } from 'swiper'; // Import Swiper core and Autoplay module
 import './Hero.css'
 import Search from '../component/Search';
-import image2 from '../asserts/images/backIcons/bg4.webp';
 import image3 from '../asserts/images/backIcons/bg2.jpg';
 import image4 from '../asserts/images/backIcons/bg3.webp';
 
@@ -15,7 +14,15 @@ const Hero = () => {
         const swiper = new Swiper('.swiper-container', {
             loop: true,
             autoplay: {
-                delay: 7000,
+                delay: 10000,
+                breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
             },
         });
     }, []);
@@ -23,7 +30,6 @@ const Hero = () => {
     return (
         <div className="swiper-container" id="Home" >
             <div className="swiper-wrapper">
-                <div className="swiper-slide" style={{ backgroundImage: `url(${image2})` }}></div>
                 <div className="swiper-slide" style={{ backgroundImage: `url(${image3})` }}></div>
                 <div className="swiper-slide" style={{ backgroundImage: `url(${image4})` }}></div>
             </div>
